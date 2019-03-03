d5b1c9fe42b1daf18ef4cbf99b1bc61e2db67807
Yanislav Igonin <jan.oasis@gmail.com>
2019-03-03 13:20:31 +0300

    Rename dist to lqr

--- src/lqr.js
+++ src/lqr.js
@@ -2,11 +2,11 @@ const { spawn } = require('child_process')
 const random = require('random-int')
 const path = require('path')
 
-// const uploadDir = path.join(__dirname, './images/upload')
+const processedDir = path.join(__dirname, '../images/processed')
 
 module.exports = (sourceImg) => {
   return new Promise((resolve, reject) => {
-    const distortedImg = path.join(__dirname, 'target.jpg')
+    const distortedImg = path.join(processedDir, path.basename(sourceImg))
 
     const factorX = random(25, 60)
     const factorY = random(25, 60)
