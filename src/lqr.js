73ba24a583459caa2b2c3e6c7559cf2c059a3baf
Yanislav Igonin <jan.oasis@gmail.com>
2019-03-03 14:12:34 +0300

    Make linting

--- src/lqr.js
+++ src/lqr.js
@@ -1,29 +1,27 @@
-const { spawn } = require('child_process')
-const random = require('random-int')
-const path = require('path')
+const { spawn } = require('child_process');
+const random = require('random-int');
+const path = require('path');
 
-const processedDir = path.join(__dirname, '../images/processed')
+const processedDir = path.join(__dirname, '../images/processed');
 
-module.exports = (sourceImg) => {
-  return new Promise((resolve, reject) => {
-    const distortedImg = path.join(processedDir, path.basename(sourceImg))
+module.exports = (sourceImg) => new Promise((resolve, reject) => {
+  const distortedImg = path.join(processedDir, path.basename(sourceImg));
 
-    const factorX = random(25, 60)
-    const factorY = random(25, 60)
+  const factorX = random(25, 60);
+  const factorY = random(25, 60);
 
-    console.log(factorX, factorY)
+  const magick = spawn('convert', [
+    sourceImg,
+    '-liquid-rescale',
+    `${factorX}x${factorY}%`,
+    '-resize',
+    `${10000 / factorX}x${10000 / factorY}%`,
+    distortedImg,
+  ]);
 
-    const magick = spawn('convert', [
-      sourceImg,
-      '-liquid-rescale', `${factorX}x${factorY}%`,
-      '-resize', `${10000 / factorX}x${10000 / factorY}%`,
-      distortedImg
-    ])
+  magick.stderr.on('data', (data) => {
+    reject(new Error(`magick error: ${data}`));
+  });
 
-    magick.stderr.on('data', (data) => {
-      reject(new Error(`magick error: ${data}`))
-    })
-
-    magick.on('close', () => resolve(distortedImg))
-  })
-}
+  magick.on('close', () => resolve(distortedImg));
+});
