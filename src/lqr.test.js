7fb20043524285502fd271181f32c0cd16c3ebc5
Yanislav Igonin <jan.oasis@gmail.com>
2019-03-03 15:25:33 +0300

    Update lqr test

--- src/lqr.test.js
+++ src/lqr.test.js
@@ -1,8 +1,14 @@
 const path = require('path');
+const fs = require('fs-extra');
 const lqr = require('./lqr');
 
 const uploadsDir = path.join(__dirname, '../images/uploaded');
 
-lqr(path.join(uploadsDir, './source.jpg'))
-  .then((target) => console.log(target))
-  .catch((err) => console.log(err));
+describe('LiquidRescale', () => {
+  test('process image', async () => {
+    const processedImage = await lqr(path.join(uploadsDir, './test_image.jpg'));
+    const isProcessedImageExist = await fs.ensureFile(processedImage);
+
+    expect(isProcessedImageExist).toBeTruthy();
+  });
+});
