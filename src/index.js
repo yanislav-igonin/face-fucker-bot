8e0d044114065c5c320ec8c048d9d4d499115669
Yanislav Igonin <jan.oasis@gmail.com>
2019-03-05 22:43:59 +0300

    Add try/catch, add bot.catch

--- src/index.js
+++ src/index.js
@@ -7,38 +7,46 @@ const lqr = require('./lqr');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const uploadsDir = path.join(__dirname, '../images/uploaded');
 
+bot.catch((err) => {
+  console.error(`ERROR: ${err}\n`);
+});
+
 bot.start((ctx) => ctx.reply('Welcome! Get fucked face in just a second!\n\n1) Кидаешь фотку\n2) ...\n3) PROFIT!'));
 
 bot.on('photo', async (ctx) => {
-  const photoInfo = await ctx.telegram.getFile(
-    ctx.update.message.photo[ctx.update.message.photo.length - 1].file_id,
-  );
-  const photoLink = await ctx.telegram.getFileLink(photoInfo.file_id);
+  try {
+    const photoInfo = await ctx.telegram.getFile(
+      ctx.update.message.photo[ctx.update.message.photo.length - 1].file_id,
+    );
+    const photoLink = await ctx.telegram.getFileLink(photoInfo.file_id);
 
-  const writer = await fs.createWriteStream(
-    path.join(uploadsDir, path.basename(photoInfo.file_path)),
-  );
+    const writer = await fs.createWriteStream(
+      path.join(uploadsDir, path.basename(photoInfo.file_path)),
+    );
 
-  const response = await axios({
-    url: photoLink,
-    method: 'GET',
-    responseType: 'stream',
-  });
+    const response = await axios({
+      url: photoLink,
+      method: 'GET',
+      responseType: 'stream',
+    });
 
-  response.data.pipe(writer);
+    response.data.pipe(writer);
 
-  response.data.on('end', async () => {
-    const sourceImage = path.join(uploadsDir, path.basename(photoInfo.file_path));
-    const processedImage = await lqr(sourceImage);
-    ctx.replyWithPhoto({ source: processedImage });
-    await fs.unlink(sourceImage);
-    await fs.unlink(processedImage);
-  });
+    response.data.on('end', async () => {
+      const sourceImage = path.join(uploadsDir, path.basename(photoInfo.file_path));
+      const processedImage = await lqr(sourceImage);
+      ctx.replyWithPhoto({ source: processedImage });
+      await fs.unlink(sourceImage);
+      await fs.unlink(processedImage);
+    });
 
-  response.data.on('error', (err) => {
-    console.error(err);
+    response.data.on('error', (err) => {
+      throw new Error(err);
+    });
+  } catch (err) {
+    console.error(`ERROR: ${err}\n`);
     ctx.reply('Something went wrong, please try again.');
-  });
+  }
 });
 
 bot.launch();
