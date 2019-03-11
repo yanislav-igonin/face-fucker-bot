31720b0048c4aa574082b735d787049bf9a4feb6
Yanislav Igonin <jan.oasis@gmail.com>
2019-03-11 10:30:04 +0000

    Resolve "Add statistics"

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
 const Telegraf = require('telegraf');
+const TelegrafMixpanel = require('telegraf-mixpanel');
 const path = require('path');
 const axios = require('axios');
 const fs = require('fs-extra');
 const lqr = require('./lqr');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const { BOT_TOKEN, MIXPANEL_TOKEN } = process.env;
+
+const bot = new Telegraf(BOT_TOKEN);
+const mixpanel = new TelegrafMixpanel(MIXPANEL_TOKEN);
+
 const uploadsDir = path.join(__dirname, '../images/uploaded');
 
 bot.catch((err) => {
   console.error(`ERROR: ${err}\n`);
 });
 
-bot.start((ctx) => ctx.reply('Welcome! Get fucked face in just a second!\n\n1) Кидаешь фотку\n2) ...\n3) PROFIT!'));
+bot.use(mixpanel.middleware());
+
+bot.start((ctx) => {
+  ctx.mixpanel.track('/start');
+  ctx.mixpanel.people.set({
+    $created: new Date().toISOString(),
+  });
+
+  ctx.reply('Welcome! Get fucked face in just a second!\n\n1) Кидаешь фотку\n2) ...\n3) PROFIT!');
+});
 
 bot.on('photo', async (ctx) => {
+  ctx.mixpanel.track('photo_uploaded');
+  ctx.mixpanel.people.set({
+    $created: new Date().toISOString(),
+  });
+
   try {
     const photoInfo = await ctx.telegram.getFile(
       ctx.update.message.photo[ctx.update.message.photo.length - 1].file_id,
@@ -38,6 +57,11 @@ bot.on('photo', async (ctx) => {
       ctx.replyWithPhoto({ source: processedImage });
       await fs.unlink(sourceImage);
       await fs.unlink(processedImage);
+
+      ctx.mixpanel.track('photo_processed');
+      ctx.mixpanel.people.set({
+        $created: new Date().toISOString(),
+      });
     });
 
     response.data.on('error', (err) => {
