7c65d2d091a45255f06da0186accfb173fd1395a
Yanislav Igonin <jan.oasis@gmail.com>
2019-04-10 16:34:45 +0300

    Add filesize checking

--- src/modules/fileLoader.js
+++ src/modules/fileLoader.js
@@ -2,8 +2,10 @@ const axios = require('axios');
 const fs = require('fs-extra');
 const path = require('path');
 const nanoid = require('nanoid');
+const filesize = require('file-size');
 
 const telegram = require('./telegram');
+const { UserError } = require('../errors');
 
 const { FOLDERS, DATA_TYPE } = require('../config');
 
@@ -26,6 +28,14 @@ const loadFile = (file_id, type) =>
     const fileInfo = await telegram.getFile(file_id);
     const fileLink = await telegram.getFileLink(file_id);
     const fileName = path.basename(fileInfo.file_path);
+
+    if (filesize(fileInfo.file_size).to('MB') > 20) {
+      throw new UserError(
+        `File size must be less than 20MB. Your uploaded file size is ${filesize(
+          fileInfo.file_size,
+        ).human('si')}`,
+      );
+    }
 
     const uniqueId = nanoid();
 
