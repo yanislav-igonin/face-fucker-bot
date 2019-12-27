013dccd6db4ff8454317653a6047c6b43b72d35c
Yanislav Igonin <jan.oasis@gmail.com>
2019-12-27 15:34:33 +0300

    Remove PhotoContextMessageUpdate interface

--- src/modules/telegram/interfaces/index.ts
+++ src/modules/telegram/interfaces/index.ts
@@ -1,9 +1,7 @@
-import { PhotoContextMessageUpdate } from './PhotoContextMessageUpdate';
 import { TextContextMessageUpdate } from './TextContextMessageUpdate';
 import { UserContextMessageUpdate } from './UserContextMessageUpdate';
 
 export {
-  PhotoContextMessageUpdate,
   TextContextMessageUpdate,
   UserContextMessageUpdate,
 };
