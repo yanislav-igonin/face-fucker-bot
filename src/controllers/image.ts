03a02f5822a1b54cbd13296e5b0aeec00724cf0e
Yanislav Igonin <jan.oasis@gmail.com>
2019-12-27 15:22:09 +0000

    Resolve "Add answer on animated sticker with error"

--- src/controllers/image.ts
+++ src/controllers/image.ts
@@ -1,6 +1,6 @@
 import { UserContextMessageUpdate } from '../modules/telegram/interfaces';
 import { userRepository } from '../modules/db/repositories';
-import { rabbit, localizator, errors } from '../modules';
+import { rabbit, localizator } from '../modules';
 import { User } from '../modules/db/entities';
 import { fileType } from '../config';
 import { files } from '../helpers';
@@ -22,11 +22,18 @@ export default async (ctx: UserContextMessageUpdate): Promise<void> => {
 
     // @ts-ignore `is_animated` does not exist, bad typings
     if (type === fileType.sticker && ctx.update.message.sticker.is_animated) {
-      throw new errors.UserError(
-        localizator(
-          user.languageCode, 'errors.animatedStickersNotSupported',
-        )(),
-      );
+      const localizedMessage = localizator(
+        user.languageCode, 'errors.animatedStickersNotSupported',
+      )();
+
+      await rabbit.publish('notificating', {
+        user,
+        messageId: ctx.update.message.message_id,
+        type: 'answer',
+        message: localizedMessage,
+      });
+
+      return;
     }
 
     await rabbit.publish('file_loading', {
