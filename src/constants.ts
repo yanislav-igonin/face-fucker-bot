d820de1cef82802659c06d786ee0700c74becff4
Yanislav Igonin <yanislavdabest@gmail.com>
2021-06-28 01:06:13 +0300

    fix: dir paths

--- src/constants.ts
+++ src/constants.ts
@@ -9,12 +9,12 @@ const fileType = {
 };
 
 const folders = {
-  imageUploads: path.join(__dirname, '../../uploads/images/uploaded'),
-  imageProcessed: path.join(__dirname, '../../uploads/images/processed'),
-  videoUploads: path.join(__dirname, '../../uploads/videos/uploaded'),
-  videoProcessed: path.join(__dirname, '../../uploads/videos/processed'),
-  videoSourceFrames: path.join(__dirname, '../../uploads/videos/sourceFrames'),
-  videoProcessedFrames: path.join(__dirname, '../../uploads/videos/processedFrames'),
+  imageUploads: path.join(__dirname, '../uploads/images/uploaded'),
+  imageProcessed: path.join(__dirname, '../uploads/images/processed'),
+  videoUploads: path.join(__dirname, '../uploads/videos/uploaded'),
+  videoProcessed: path.join(__dirname, '../uploads/videos/processed'),
+  videoSourceFrames: path.join(__dirname, '../uploads/videos/sourceFrames'),
+  videoProcessedFrames: path.join(__dirname, '../uploads/videos/processedFrames'),
 };
 
 const processFactor = {
