84d1859a6f486d039a7e7e96b24da3d7cfb65b9a
Yanislav Igonin <yanislavdabest@gmail.com>
2021-06-27 16:48:48 +0300

    feat: update start controller

--- src/modules/bot/controllers/start.controller.ts
+++ src/modules/bot/controllers/start.controller.ts
@@ -1,5 +1,13 @@
-import { Context } from 'telegraf';
+import { Composer } from 'telegraf';
+import { userRepository } from '../../db/repositories';
+import { localizator } from '../..';
 
-export const StartController = async (ctx: Context) => {
-  ctx.reply(`${new Date().toLocaleString()} - start`);
-};
+export const StartController = Composer.on('text', async (ctx) => {
+  let user = await userRepository.getUser(ctx.update.message.from.id);
+  if (user === undefined) {
+    user = await userRepository.createUser(ctx.update.message.from);
+  }
+
+  const localizedMessage = localizator(user.languageCode, 'start')();
+  ctx.reply(localizedMessage);
+});
