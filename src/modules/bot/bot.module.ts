e16ed0238298baf81fde2405eb42e7ea0fa9c851
Yanislav Igonin <yanislavdabest@gmail.com>
2023-06-21 17:45:32 +0300

    disable webhook

--- src/modules/bot/bot.module.ts
+++ src/modules/bot/bot.module.ts
@@ -49,17 +49,23 @@ export class BotModule {
   async launch() {
     const { app, telegram } = this.config;
 
-    let host;
-    if (app.env === 'development') {
-      host = await ngrok.connect(telegram.webhook.port);
-    } else {
-      // eslint-disable-next-line prefer-destructuring
-      host = telegram.webhook.host;
-    }
+    if (telegram.webhook.isEnabled) {
+      let host;
+      if (app.env === 'development') {
+        host = await ngrok.connect(telegram.webhook.port);
+      } else {
+        // eslint-disable-next-line prefer-destructuring
+        host = telegram.webhook.host;
+      }
 
-    await this.bot.telegram.deleteWebhook();
-    const url = `${host}${telegram.webhook.path}`;
-    await this.bot.telegram.setWebhook(url);
-    logger.info('bot - online');
+      await this.bot.telegram.deleteWebhook();
+      const url = `${host}${telegram.webhook.path}`;
+      await this.bot.telegram.setWebhook(url);
+      logger.info('bot - online');
+    } else {
+      await this.bot.telegram.deleteWebhook();
+      await this.bot.launch();
+      logger.info('bot - online');
+    }
   }
 }
